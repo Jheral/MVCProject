01e6725d2fdf71048e6196f940fbe1b47a66540b
Jheral <ericl87@hotmail.com>
2015-03-16 09:16:45 +0100

    Updated Register page

--- MVCProject/MVCProject/Models/AccountViewModels.cs
+++ MVCProject/MVCProject/Models/AccountViewModels.cs
@@ -63,10 +63,10 @@ namespace MVCProject.Models
 
     public class RegisterViewModel
     {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+		[Required]
+		[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+		[Display(Name = "User Name")]
+		public string UserName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
@@ -78,6 +78,19 @@ namespace MVCProject.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+		[Required]
+		[Display(Name = "First Name")]
+		public string FirstName { get; set; }
+
+		[Required]
+		[Display(Name = "Last Name")]
+		public string LastName { get; set; }
+
+		[Required]
+		[EmailAddress]
+		[Display(Name = "Email")]
+		public string Email { get; set; }
     }
 
     public class ResetPasswordViewModel
